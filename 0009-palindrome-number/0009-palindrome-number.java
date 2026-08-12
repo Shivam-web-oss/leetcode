d6b658631fc152ecc762f4085046ae1795aca9fc
Ace Art <153087670+Shivam-web-oss@users.noreply.github.com>
2026-08-12 14:31:45 +0530

    Time: 6 ms (21.42%), Space: 46.1 MB (33.64%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x<0){
+            return false;
+        }
+        char[] arr = String.valueOf(x).toCharArray();
+        int left=0;
+        int right= arr.length-1;
+        while (left<right){
+            if (arr[left]!=arr[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
