f8208d11fbb98e1e7865c581d43110251a3fdd58
Ace Art <153087670+Shivam-web-oss@users.noreply.github.com>
2026-08-28 17:55:26 +0530

    Time: 1 ms (100%), Space: 47.2 MB (52.17%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,24 +1,34 @@
-import java.util.HashMap;
-import java.util.Arrays;
-
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        HashMap<Integer, String> map = new HashMap<>();
+        qs(names,heights,0,heights.length-1);
+        return names;
 
-        for (int i = 0; i < names.length; i++) {
-            map.put(heights[i], names[i]);
+    }
+    public void qs(String[] names,int heights[],int low,int high){
+        if(low>=high) return;
+        int start=low;
+        int end=high;
+        int mid=start+(end-start)/2;
+        int pivot=heights[mid];
+        while(start<=end){
+            while(heights[start]>pivot){
+                start++;
+            }
+            while(heights[end]<pivot){
+                end--;
+            }
+            if(start<=end){
+                int temp=heights[start];
+                heights[start]=heights[end];
+                heights[end]=temp;
+                String s=names[start];
+                names[start]=names[end];
+                names[end]=s;
+                start++;
+                end--;
+            }
         }
-
-        // Sort in ascending order
-        Arrays.sort(heights);
-
-        // Fill result array in reverse (descending order)
-        String[] result = new String[names.length];
-        int n = heights.length;
-        for (int i = 0; i < n; i++) {
-            result[i] = map.get(heights[n - 1 - i]);
-        }
-
-        return result;
+        qs(names,heights,low,end);
+        qs(names,heights,start,high);
     }
 }
