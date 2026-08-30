140758b37472b3c1850213ea8e41f73b81d56705
Ace Art <153087670+Shivam-web-oss@users.noreply.github.com>
2026-08-30 15:57:55 +0530

    Time: 1 ms (99.45%), Space: 46.7 MB (90.44%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,31 +1,18 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        int possibleDups = 0;
-        int length_ = arr.length - 1;
-
-        // Count zeros to determine shift boundary
-        for (int left = 0; left <= length_ - possibleDups; left++) {
-            if (arr[left] == 0) {
-                // Edge case: zero cannot be duplicated because it's at the boundary
-                if (left == length_ - possibleDups) {
-                    arr[length_] = 0;
-                    length_--;
-                    break;
-                }
-                possibleDups++;
+        int n = arr.length;
+        int[] res= Arrays.copyOf(arr, n);
+        int i=0;
+        int j=0;
+        while(i<n && j<n){
+            arr[j] = res[i];
+            j++;
+            if (res[i] == 0 && j < n) {
+                arr[j] = 0;
+                j++;
             }
+            i++;
         }
-
-        // Copy elements backwards
-        int last = length_ - possibleDups;
-        for (int i = last; i >= 0; i--) {
-            if (arr[i] == 0) {
-                arr[i + possibleDups] = 0;
-                possibleDups--;
-                arr[i + possibleDups] = 0;
-            } else {
-                arr[i + possibleDups] = arr[i];
-            }
-        }
+        return;
     }
 }
