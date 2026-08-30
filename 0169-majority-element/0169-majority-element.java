d26178d58a88cc8d5d997a417ed0fec70815fafc
Ace Art <153087670+Shivam-web-oss@users.noreply.github.com>
2026-08-30 18:11:50 +0530

    Time: 1 ms (99.89%), Space: 56 MB (6.66%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int ans=nums[0],c=1;
+    for (int i=1;i<nums.length;i++){
+        if(c==0){
+            ans=nums[i];
+            c=1;
+        }else if(nums[i]==ans){
+            c++;
+        }else{
+            c--;
+        }
+    }
+    return ans;
+    }
+}
