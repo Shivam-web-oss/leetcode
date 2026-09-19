9fdeccf4f96a85d775ecf4b952544c69d6238b4e
Ace Art <153087670+Shivam-web-oss@users.noreply.github.com>
2026-09-19 21:09:23 +0530

    Time: 4 ms (41.01%), Space: 12.5 MB (73.49%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack=[]
+        bracketMap={')':'(','}':'{',']':'['}
+        for char in s:
+            if char in bracketMap:
+                top=stack.pop() if stack else '#'
+                if bracketMap[char] != top:
+                    return False
+            else:
+                stack.append(char)
+        return not stack        
